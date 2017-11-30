1945efbc73d20cd508cde9aa62a930d29835e4e3
Peaches <caroline.memishian@vanderbilt.edu>
2017-11-30 10:41:18 -0600

    blah2

--- app/src/main/java/cs251/vandy/principlesofsoftwareengineering/MapsActivity.java
+++ app/src/main/java/cs251/vandy/principlesofsoftwareengineering/MapsActivity.java
@@ -1,7 +1,8 @@
 package cs251.vandy.principlesofsoftwareengineering;
 
-import android.support.v4.app.FragmentActivity;
+//import android.support.v4.app.FragmentActivity;
 import android.os.Bundle;
+import android.support.v7.app.AppCompatActivity;
 
 import com.google.android.gms.maps.CameraUpdateFactory;
 import com.google.android.gms.maps.GoogleMap;
@@ -10,9 +11,10 @@ import com.google.android.gms.maps.SupportMapFragment;
 import com.google.android.gms.maps.model.LatLng;
 import com.google.android.gms.maps.model.MarkerOptions;
 
-public class MapsActivity extends FragmentActivity implements OnMapReadyCallback {
+//was extends FragmentActivity
+public class MapsActivity extends AppCompatActivity implements OnMapReadyCallback {
 
-    private GoogleMap mMap;
+   // private GoogleMap mMap;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -35,28 +37,29 @@ public class MapsActivity extends FragmentActivity implements OnMapReadyCallback
      */
     @Override
     public void onMapReady(GoogleMap googleMap) {
-        mMap = googleMap;
+       // mMap = googleMap;
 
         LatLng appleMarket = new LatLng(36.057755, -86.717139);
-        mMap.addMarker(new MarkerOptions().position(appleMarket).title("Marker at Apple Market"));
-        mMap.moveCamera(CameraUpdateFactory.newLatLng(appleMarket));
+        googleMap.addMarker(new MarkerOptions().position(appleMarket).title("Marker at Apple Market"));
+        googleMap.moveCamera(CameraUpdateFactory.newLatLng(appleMarket));
 
-        LatLng bellshireValPlus = new LatLng(36.266201, -86.756205);
-        mMap.addMarker(new MarkerOptions().position(bellshireValPlus).title("Marker at Bellshire Value Plus"));
-        mMap.moveCamera(CameraUpdateFactory.newLatLng(bellshireValPlus));
+        /**LatLng bellshireValPlus = new LatLng(36.266201, -86.756205);
+         mMap.addMarker(new MarkerOptions().position(bellshireValPlus).title("Marker at Bellshire Value Plus"));
+         mMap.moveCamera(CameraUpdateFactory.newLatLng(bellshireValPlus));
 
 
-        LatLng billMartins = new LatLng(36.173850, -86.750144);
-        mMap.addMarker(new MarkerOptions().position(billMartins).title("Marker at Bill Martins"));
-        mMap.moveCamera(CameraUpdateFactory.newLatLng(billMartins));
+         LatLng billMartins = new LatLng(36.173850, -86.750144);
+         mMap.addMarker(new MarkerOptions().position(billMartins).title("Marker at Bill Martins"));
+         mMap.moveCamera(CameraUpdateFactory.newLatLng(billMartins));
 
-        LatLng comptons = new LatLng(36.089303, -86.609406);
-        mMap.addMarker(new MarkerOptions().position(comptons).title("Marker at Compton's Foodland"));
-        mMap.moveCamera(CameraUpdateFactory.newLatLng(comptons));
+         LatLng comptons = new LatLng(36.089303, -86.609406);
+         mMap.addMarker(new MarkerOptions().position(comptons).title("Marker at Compton's Foodland"));
+         mMap.moveCamera(CameraUpdateFactory.newLatLng(comptons));
 
-        LatLng durdur = new LatLng(36.127986, -86.711134);
-        mMap.addMarker(new MarkerOptions().position(durdur).title("Marker at Dur Dur International Market"));
-        mMap.moveCamera(CameraUpdateFactory.newLatLng(durdur));
+         LatLng durdur = new LatLng(36.127986, -86.711134);
+         mMap.addMarker(new MarkerOptions().position(durdur).title("Marker at Dur Dur International Market"));
+         mMap.moveCamera(CameraUpdateFactory.newLatLng(durdur));
+         **/
     }
 }
 
